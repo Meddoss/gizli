f4acc745cbc8c4567615f159148cd079133e1cc7
talibov44 <88476710+talibov44@users.noreply.github.com>
2021-08-05 20:19:58 +0400

    Add files via upload

--- admins.py
+++ admins.py
@@ -1,76 +1,15 @@
-from asyncio.queues import QueueEmpty
-
-from pyrogram import Client
-from pyrogram.types import Message
-
-import callsmusic
-
-from config import BOT_NAME as BN
-from helpers.filters import command, other_filters
-from helpers.decorators import errors, authorized_users_only
+from typing import List, Dict, Union
 
 
-@Client.on_message(command("durdur") & other_filters)
-@errors
-@authorized_users_only
-async def durdur(_, message: Message):
-    if (
-            message.chat.id not in callsmusic.pytgcalls.active_calls
-    ) or (
-            callsmusic.pytgcalls.active_calls[message.chat.id] == 'Durduruldu'
-    ):
-        await message.reply_text(f"**{BN} :-** 🙄 Müzik açık değil!")
-    else:
-        callsmusic.pytgcalls.pause_stream(message.chat.id)
-        await message.reply_text(f"**{BN} :-** 🤐 Durduruldu!")
+admins: Dict[int, List[int]] = {}
 
 
-@Client.on_message(command("devam") & other_filters)
-@errors
-@authorized_users_only
-async def devam(_, message: Message):
-    if (
-            message.chat.id not in callsmusic.pytgcalls.active_calls
-    ) or (
-            callsmusic.pytgcalls.active_calls[message.chat.id] == 'Devam Ediyor'
-    ):
-        await message.reply_text(f"**{BN} :-** 🙄 Hiçbirşey Durdurulmadı!")
-    else:
-        callsmusic.pytgcalls.resume_stream(message.chat.id)
-        await message.reply_text(f"**{BN} :-** 🥳 Devam Ediyor!")
+def set(chat_id: int, admins_: List[int]):
+    admins[chat_id] = admins_
 
 
-@Client.on_message(command("bitir") & other_filters)
-@errors
-@authorized_users_only
-async def bitir(_, message: Message):
-    if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text(f"**{BN} :-** 🙄 Hiçbirşey Oynatılmıyor!")
-    else:
-        try:
-            callsmusic.queues.clear(message.chat.id)
-        except QueueEmpty:
-            pass
+def get(chat_id: int) -> Union[List[int], bool]:
+    if chat_id in admins:
+        return admins[chat_id]
 
-        callsmusic.pytgcalls.leave_group_call(message.chat.id)
-        await message.reply_text(f"**{BN} :-** ❌ Bitirildi!")
-
-
-@Client.on_message(command("atla") & other_filters)
-@errors
-@authorized_users_only
-async def atla(_, message: Message):
-    if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text(f"**{BN} :-** 🙄 Atlamak için hiçbir şey oynamıyor!")
-    else:
-        callsmusic.queues.task_done(message.chat.id)
-
-        if callsmusic.queues.is_empty(message.chat.id):
-            callsmusic.pytgcalls.leave_group_call(message.chat.id)
-        else:
-            callsmusic.pytgcalls.change_stream(
-                message.chat.id,
-                callsmusic.queues.get(message.chat.id)["file_path"]
-            )
-
-        await message.reply_text(f"**{BN} :-** 😬 Bir Sonraki Müziğe Atlandı!")
+    return False
